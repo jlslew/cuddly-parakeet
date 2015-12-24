083d823726c2c9c8626b7a324b7295db0c01fcf0
Jonathan Lee Slew <jonathan.leeslew@sympatico.ca>
2015-12-24 17:22:46 +0000

    Added Hello World! example

---
+++ app/main.js
@@ -0,0 +1,7 @@
+require("../bower_components/uikit/css/uikit.gradient.css");
+require("./styles/style.css");
+
+require("../bower_components/jquery/dist/jquery.js");
+require("../bower_components/uikit/js/uikit.js");
+
+require("./assets/webpack.html");
